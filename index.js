6183b7ccc659b6d777a03768d5a19fa2f50a67e5
Ellis Lunayo <ellislunayo@gmail.com>
2025-03-18 21:32:37 +0300

    FAUNCTIONS eXTENDED

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(activity="roller-skate!"){
+    return `This Saturday, I want to ${activity}`;
+}
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity='go to the office'){
+    return `This Monday, I will ${activity}.`;
+}
+
+// function mondayWork(activity='work from home.'){
+    
+// }
+
+function wrapAdjective(flair = "*") {
+    return function(adjective) {
+        return `You are ${flair}${adjective}${flair}!`;
+    };
+}
